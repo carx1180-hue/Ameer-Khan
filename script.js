10897c2b2b87ab3820b29178b612046b70c1694a
carx1180-hue <carx1180@gmail.com>
2025-09-03 17:44:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    function scrollLeft() {
+      document.getElementById('carouselTrack').scrollBy({left: -200, behavior: 'smooth'});
+    }
+    function scrollRight() {
+      document.getElementById('carouselTrack').scrollBy({left: 200, behavior: 'smooth'});
+    }
+  </script>
